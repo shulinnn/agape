ad3e4f2e6d4d238aca2bb66b06ebdd3b4c37d81b
Patrik Daňo <49112352+shulinnn@users.noreply.github.com>
2022-12-19 00:45:53 +0100

    add navbar component

--- src/partials/navbar/Navbar.js
+++ src/partials/navbar/Navbar.js
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import { Menu, X } from "react-feather";
+
+import "./Navbar.scss";
+
 function Navbar() {
-  return "LOL";
+  const [isOpen, setIsOpen] = useState(false);
+
+  ///
+  /// Comment comes here
+  ///
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-wrapper">
+          <span>AGAPE</span>
+          {!isOpen ? (
+            <Menu
+              color="black"
+              size={32}
+              strokeWidth={1}
+              onClick={handleToggle}
+            />
+          ) : (
+            <X color="black" size={32} strokeWidth={1} onClick={handleToggle} />
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
